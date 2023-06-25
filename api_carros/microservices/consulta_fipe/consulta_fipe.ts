546e5148b32f8fda453ea263701e1e8389b8bfca
Maicon Businari <maiconlucascamposbusinari@gmail.com>
2023-06-24 23:28:19 -0300

    Inplementado Buscas externa como microserviços

---
+++ api_carros/microservices/consulta_fipe/consulta_fipe.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import amqp from "amqplib";
+import { Carro } from "../../model/carro_model";
+import { PrismaClient } from "@prisma/client";
+import axios from 'axios';
+
+
+
+const app = express();
+const port = 6005;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/consultaFipe/:id", async (req, res) => {
+
+    const prisma = new PrismaClient();
+    const id = parseInt(req.params.id);
+    
+    const carro = await prisma.carro.findUnique(
+        {
+            where: {
+                id: id
+            }
+        }
+    )
+
+    if(!carro){
+        return res.status(200).json({
+            message: "Carro não encontrado",
+        })
+    }
+
+    await axios.get(`https://brasilapi.com.br/api/fipe/preco/v1/${carro.codigoFipe}`)
+    .then( (sucesso) => {
+        return res.status(200).json({
+            Fipe: sucesso.data[1]
+        })
+    })
+    .catch( () => {
+        return res.status(400).json({
+            message: "Verifique o se está correto o codigo da fipe."
+        })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor de consulta fipe de carros na porta ${port}`);
+});
+
